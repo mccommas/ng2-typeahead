84e90ebd18f34eb213284d65900525207a0e56e4
Jeffrey A McCommas <jeffmccommas@gmail.com>
2017-01-18 00:53:53 +0530

    typhead demo draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { TypeaheadModule } from 'ng2-bootstrap';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    TypeaheadModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
